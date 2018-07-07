da5f744180815d1da8aba54037a64feb46d54ede
MiguelAngel0201 <mpena.creactiva@gmail.com>
2018-07-07 17:26:04 -0400

    Optimizacion de pagina de recoleccion y archivo js de ella

--- PaginaDeRecoleccion.php
+++ PaginaDeRecoleccion.php
@@ -12,142 +12,81 @@
 </head>
 
 <body>
-
     <div class="container">
         <div class="row">
             <div class="col">
                 <h1 class="text-center text-primary">Generar Presupuesto</h1>
             </div>
-
             <div class="w-100"></div>
-
-
             <div class="col-12">
-                <!--
-        #forajax: Formulario Ajax NO LO PUDE HACER
-        #PhpPost HECHO
-    --->
                 <form action="PaginaDeImpresion.php" method="POST">
                     <div class="from-group mt-5">
                         <h3>Ingresar Fecha</h3>
-                        <!--
-                #Fecha
-            --->
-                        <!--
-                Los inputs numéricos no pueden ser limitados por maxlength, usar funcion substr() para limitar los caracteres 
-            --->
-                        <input maxlength="8" type="date" class="form-control-sm" placeholder="DD/MM/AA" name="Fecha" id="Fecha" required>
+                        <!--#Fecha--->
+                        <input maxlength="8" type="date" class="form-control-sm" name="Fecha" id="Fecha" required>
                     </div>
-
                     <div class="from-group mt-4">
                         <h3>Nombre de Cliente</h3>
-
-                        <!--
-                #NC: Nombre Del Cliente
-            --->
+                        <!--#NC: Nombre Del Cliente--->
                         <input maxlength="95" type="text" class="form-control" placeholder="Nombre" Name="NC" id="NC " required>
                     </div>
-
                     <div class="from-group mt-4">
                         <h3>Nombre del proyecto</h3>
-
-                        <!--
-                #NP:Nombre del Proyecto
-            --->
+                        <!--#NP:Nombre del Proyecto--->
                         <input maxlength="95" type="text" class="form-control" placeholder="Nombre de proyecto" Name="NP" id="NP" required>
                     </div>
                     <h2 class="mt-5 text-primary">Ingrese datos del trabajo realizado</h2>
 
-                    <div id="DatosTabla">
-                        <div class="from-group mt-4">
-                            <h3>Nombre del dato y desarrollo de descripción de dato realizado</h3>
-                            <!--
-                <h6>Nota: ingrese esta etiqueta "br/" entre estas comillas españolas "<>". si desea hacer un salto de linea por favor no agreguen mas de 5-6 saltos.</h6>
-                --->
-                            <!--
-                    #NDT: Nombre del dato de trabajo
-                --->
-                            <textarea maxlength="240" type="text" class="form-control mb-3" placeholder="Dato del trabajo1" Name="NDT1" id="NDT1" required></textarea>
-                            <!--MAQUETACION DE IDEA-->
-                            <textarea id="delNDT2" class="d-none" name="NDT2"></textarea>
-                            <textarea id="delNDT3" class="d-none" name="NDT3"></textarea>
-                            <textarea id="delNDT4" class="d-none" name="NDT4"></textarea>
-                            <textarea id="delNDT5" class="d-none" name="NDT5"></textarea>
-                            <textarea maxlength="550" type="text" class="form-control mb-3" placeholder="Descripción1" Name="DT1" id="DT1" required></textarea>
-                            <!--MAQUETACION DE IDEA-->
-                            <textarea id="delDT2" class="d-none" name="DT2"></textarea>
-                            <textarea id="delDT3" class="d-none" name="DT3"></textarea>
-                            <textarea id="delDT4" class="d-none" name="DT4"></textarea>
-                            <textarea id="delDT5" class="d-none" name="DT5"></textarea>
-
-                            <button id="MasDatos" type="button" class="btn btn-outline-primary">Agregar más datos del trabajo</button>
-
-                            <button id="MenosDatos" type="button" class="btn btn-outline-danger">Eliminar datos</button>
+                    <!--#NDT: Nombre del dato de trabajo-->
+                    <div class="form-row mt-4">
+                        <div class="form-group col-md-4">
+                            <h4>Nombre del dato de trabajo</h4>
+                            <textarea maxlength="240" type="text" class="ndt1 form-control mb-2" placeholder="Dato del trabajo1" Name="NDT1" required></textarea>
                         </div>
-
-                        <div id="Informacion" class="from-group mt-4">
-                            <h3>Introducir información adicional</h3>
-
-                            <!--
-                .ia: información adicional
-                #BS: Boton safe
-            --->
-                            <textarea maxlength="210" type="text" class="ia1 form-control mb-3" placeholder="Información Adicional" Name="IA1" required></textarea>
-                            <!--Textareas ocultos envían información en blanco-->
-                            <textarea id="DelIA2" class="d-none" name="IA2"></textarea>
-                            <textarea id="DelIA3" class="d-none" name="IA3"></textarea>
-
-
+                        <div class="form-group col-md-8">
+                            <h4>Introduzca dato de trabajo</h4>
+                            <textarea maxlength="550" type="text" class="dt1 form-control mb-2" placeholder="Descripción1" Name="DT1" required></textarea>
                         </div>
-
-                        <div id="despues"></div>
-                        <button id="MasInformacion" type="button" class="btn btn-outline-primary">Agregar más información</button>
-                        <button id="MenosInfo" type="button" class="btn btn-outline-danger">Eliminar información</button>
                         <div class="w-100"></div>
+                        <button id="MasDatos" type="button" class="btn btn-outline-primary mr-1">Agregar más datos del trabajo</button>
+                        <button id="MenosDatos" type="button" class="btn btn-outline-danger">Eliminar datos</button>
+                    </div>
 
-                        <div class="from-group mt-4">
-                            <h3>Ingresar el balance total del presupuesto</h3>
 
-                            <!--
-                BT: Balance Total
-            --->
-                            <input maxlength="20" type="number" class="form-control-sm mb-3" placeholder="XXXX" Name="BT" id="BT" required onKeyDown="Contar()" onKeyUp="Contar()">
-                            <!-- Contador retroceso de caracteres
-                        <input type="text" id="Cont" value="15" >
-                        -->
-                            <h5>Moneda</h5>
-                            <!--Estructura del label
-                        <label for="element_id">
-                        
-                        --->
-                            <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" id="BsF" name="Moneda" class="custom-control-input" value="BsF" required>
-                                <label class="custom-control-label" for="BsF">BsF</label>
-                            </div>
-
-                            <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" id="BsS" name="Moneda" class="custom-control-input" value="BsS" required>
-                                <label class="custom-control-label" for="BsS">BsS</label>
-                            </div>
-
-                            <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" id="Dolar" name="Moneda" class="custom-control-input" value="$" required>
-                                <label class="custom-control-label" for="Dolar">Dolar</label>
-                            </div>
-
+                    <div id="Informacion" class="from-group mt-4">
+                        <h3>Introducir información adicional</h3>
+                        <!--.ia: información adicional;--->
+                        <textarea maxlength="210" type="text" class="ia1 form-control mb-3" placeholder="Información Adicional" Name="IA1" required></textarea>
+                    </div>
+                    <button id="MasInformacion" type="button" class="btn btn-outline-primary">Agregar más información</button>
+                    <button id="MenosInfo" type="button" class="btn btn-outline-danger">Eliminar información</button>
+                    <div class="w-100"></div>
+                    <div class="from-group mt-4">
+                        <h3>Ingresar el balance total del presupuesto</h3>
+                        <!--BT: Balance Total--->
+                        <input maxlength="20" type="number" class="form-control-sm mb-3" placeholder="XXXX" Name="BT" id="BT" required onKeyDown="Contar()" onKeyUp="Contar()">
+                        <h5>Moneda</h5>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" id="BsF" name="Moneda" class="custom-control-input" value="BsF" required>
+                            <label class="custom-control-label" for="BsF">BsF</label>
                         </div>
-
-                        <input type="submit" class="btn btn-primary mt-4" name="submit" id="BS" value="Clickear para generar presupuesto">
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" id="BsS" name="Moneda" class="custom-control-input" value="BsS" required>
+                            <label class="custom-control-label" for="BsS">BsS</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" id="Dolar" name="Moneda" class="custom-control-input" value="$" required>
+                            <label class="custom-control-label" for="Dolar">Dolar</label>
+                        </div>
+                    </div>
+                    <input type="submit" class="btn btn-primary mt-4" name="submit" id="BS" value="Clickear para generar presupuesto">
                 </form>
-                </div>
-
-
             </div>
         </div>
-
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery-3.3.1.min.js"></script>
-        <script src="js/recoleccion.js"></script>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/recoleccion.js"></script>
 </body>
 
 </html>
--- js/recoleccion.js
+++ js/recoleccion.js
@@ -1,89 +1,12 @@
-/*Duplicador de textarea "Información adicional"*/
-var contInfo = 1;
-var $klonInfo;
-$(document).ready(function () {
-
-    $("#MasInformacion").click(function () {
-        if (contInfo < 3) { 
-            contInfo++;
-            //Selector class="NC"
-            var $Informacion = $('textarea[class^="ia"]:last');
-
-            //Parse: Analizar gramaticalmente; Int: enteros
-            //La función parseInt, analiza un texto y obtiene los valores integer
-            //meta aracter /\d/g : Da valor numérico del  0 al 9 separado en ","
-            //meta caracter /\D/g : Da valor char separado en ","
-            //.prop obtiene el valor de la propiedad
-            var num = parseInt($Informacion.prop("class").match(/\d/g)) + 1;
-
-            //el metodo .attr() puede editar los atributos en el html 
-            $("#DelIA" + contInfo).remove();
-            
-            //val('') edita el value del input
-            $klonInfo = $Informacion.clone().attr({
-                name: 'IA' + num,
-                class: 'ia' + num + ' form-control mb-3'
-            }).val('');
-            
-            //el metodo .after() coloca contenido después de elementos seleccionados
-            $('header #CodBar').append('<p>Hola<p>');
-            $Informacion.after($klonInfo);
-        } else {
-            alert("Límite excedido");
-        }
-    });
-
-    $('#MenosInfo').click(function () {
-        if (contInfo > 1) {
-            //Remueve la class que seleccioné y le resta al contador del inputs
-            
-            $kloneInfo = $('textarea[name ^= "IA"]:last').clone().attr({
-                name: 'IA' + contInfo,
-                class: 'd-none',
-                id: 'DelIA' + contInfo  
-            }).val('');
-            
-            $('textarea[class^="ia"]:last').remove();
-            
-            $('textarea[class^="ia"]:last').after($kloneInfo);
-            contInfo--;
-        } else {
-            alert('Límite de reducción excedido');
-        }
-    });
-
-});
-
-
-
-/*Duplicador de textarea "Datos del trabajo realizado"*/
+/**********Duplicador de textarea "Datos del trabajo realizado"**********/
 var contMasInfo = 1;
 $(document).ready(function () {
     $("#MasDatos").click(function () {
-        if (contMasInfo <= 4) {
+        if ((contMasInfo >= 1 && contMasInfo < 10)){
             contMasInfo++;
-            var $NDT = $('textarea[id ^= "NDT"]:last');
-            var $DT = $('textarea[id ^= "DT"]:last');
-            
-            $('#delNDT' + contMasInfo).remove();
-            $('#delDT' + contMasInfo).remove();
-            
-            var NUM = parseInt($NDT.prop("id").match(/\d/g)) + 1;
-
-            var $ClonarNDT = $NDT.clone().attr({
-                name: 'NDT' + NUM,
-                id: 'NDT' + NUM,
-                placeholder: 'Dato del trabajo' + NUM
-            }).val('');
-
-            var $ClonarDT = $DT.clone().attr({
-                name: 'DT' + NUM,
-                id: 'DT' + NUM,
-                placeholder: 'Descripción' + NUM
-            }).val('');
-
-            $NDT.after($ClonarNDT);
-            $DT.after($ClonarDT);
+            var NDATO = '<textarea maxlength="240" type="text" class="ndt' + contMasInfo + ' form-control mb-2" placeholder="Dato del trabajo' + contMasInfo + '" Name="NDT' + contMasInfo + '" required></textarea>';
+            var DATO = '<textarea maxlength="550" type="text" class="dt'+ contMasInfo + ' form-control mb-2" placeholder="Descripción'+ contMasInfo + '" Name="DT'+ contMasInfo + '" required></textarea>';
+            $('textarea[name ^= "NDT"]:last').after(NDATO);$('textarea[name ^= "DT"]:last').after(DATO);
         } else {
             alert("Límite excedido");
         }
@@ -91,31 +14,37 @@ $(document).ready(function () {
 
     $("#MenosDatos").click(function () {
         if (contMasInfo > 1) {
-            $('textarea[id ^= "NDT"]:last').remove();
-            $('textarea[id ^= "DT"]:last').remove();
-            
-            var ClonardelNDT =  $('textarea[name ^= "NDT"]:last').clone().attr({
-                name: 'NDT' + contMasInfo,
-                id: 'delNDT' + contMasInfo,
-                class: 'd-none'
-            }).val('');
-            var ClonardelDT =  $('textarea[name ^= "DT"]:last').clone().attr({
-                name: 'DT' + contMasInfo ,
-                id: 'delDT' + contMasInfo,
-                class: 'd-none'
-            }).val('');
-            
-            $('textarea[id ^= "NDT"]:last').after(ClonardelNDT);
-            $('textarea[id ^= "DT"]:last').after(ClonardelDT);
+            $('textarea[name ^= "DT"]:last, textarea[name ^= "NDT"]:last').remove();
             contMasInfo--;
         } else {
             alert('Límite de reducción excedido');
         }
     });
 });
+/**********Duplicador de textarea "Información adicional"**********/
+var contInfo = 1;
+$(document).ready(function () {
+    $("#MasInformacion").click(function () {
+        if ((contInfo >= 1) && (contInfo <= 10)) {
+            contInfo++;
+            var INFO = '<textarea maxlength="210" type="text" class="ia' + contInfo + ' form-control mb-3" placeholder="Información Adicional" Name="IA' + contInfo + '" required></textarea>';
+            $('textarea[class ^= "ia"]:last').after(INFO).val('');
+        } else {
+            alert('Límite excedido');
+        }
+    });
 
+    $('#MenosInfo').click(function () {
+        if (contInfo > 1) {
+            $('textarea[class ^= "ia"]:last').remove();
+            contInfo--;
+        } else {
+            alert('Límite de reducción excedido');
+        }
+    });
 
-/*Limitador de caracteres en input numérico*/
+});
+/**********Limitador de caracteres en input numérico**********/
 function Contar() {
     var max = 15;
     var cadena = document.getElementById("BT").value;
@@ -128,4 +57,3 @@ function Contar() {
         document.getElementById("BT").value = cadena.substr(0, max);
     }
 }
-/*MAQUETACION DE IDEA. CUANDO SE DUPLIQUE EL FORMULARIO, SE ELIMINARA LOS ID "Del" correspondiente al nùmero de inputs creados */
